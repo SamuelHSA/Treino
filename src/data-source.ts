e9a75ecd77c409a7b6f9caac2c311ae0737ff1fc
SamuelHSA <longimusplay@gmail.com>
2023-01-30 20:33:38 -0300

    Arrumando a api

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,10 +4,10 @@ import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "127.0.0.1",
     port: 3306,
     username: "root",
-    password: "developer",
+    password: "root1",
     database: "scoder",
     entities: [User],
     migrations: [],
--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,21 @@ const express = require('express')
 const app = express();
 
 
-// AppDataSource.initialize().then(async () => {
+AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
     const user = new User()
-    user.name = "Timber"
+    user.name = "Alek"
     user.email = "dev@dev.com"
     user.password = "1234"
     // await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    // console.log("Saved a new user with id: " + user.id)
 
-    console.log("Loading users from the database...")
+    // console.log("Loading users from the database...")
     // const users = await AppDataSource.manager.find(User)
     // console.log("Loaded users: ", users)
 
-// }).catch(error => console.log(error))
+}).catch(error => console.log(error))
 
 
 app.use(express.json());
